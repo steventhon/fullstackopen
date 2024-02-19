03ba679c68924106ebd3918a63e6cd1d6c5496e3
Steven Thon <steventhon94@gmail.com>
2024-02-19 15:41:38 -0800

    Complete 2.9, add a search field to filter the list of people by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,8 +6,12 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [filterName, setFilterName] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   
@@ -22,12 +26,18 @@ const App = () => {
       const personObject = {
         name: newName,
         number: newNumber,
+        id: person.length + 1,
       }
     
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
     }
+  }
+
+  const handleFilterNameChange = (event) => {
+    console.log(event.target.value)
+    setFilterName(event.target.value)
   }
 
   const handleNameChange = (event) => {
@@ -40,9 +50,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const personsToShow = filterName.length === 0
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with 
+        <input
+          value={filterName}
+          onChange={handleFilterNameChange}
+        />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: 
@@ -64,11 +86,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
-          <Person key={person.name} person={person} />
+        {personsToShow.map(person => 
+          <Person key={person.id} person={person} />
         )}
       </ul>
-      <div>debug newName: {newName}</div>
+      <div>debug: {newName} {newNumber}</div>
     </div>
   )
 }
