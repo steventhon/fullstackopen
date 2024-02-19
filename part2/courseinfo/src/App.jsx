67e440e3ef849ea7bc288e75f74578669d9d1e46
Steven Thon <steventhon94@gmail.com>
2024-02-19 00:19:18 -0800

    Update Content to show parts regardless of the number of parts

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,49 +1,60 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
 
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </>
+  )
+}
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Course = (props) => {
+  return (
+    <>
+      <Header text={props.course.name} />
+      <Content parts={props.course.parts} />
+    </>
+  )
+}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
--- part2/courseinfo/src/main.jsx
+++ part2/courseinfo/src/main.jsx
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
 )
