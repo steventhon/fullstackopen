d4579c650fd691b284c7e05fffb41af8b113d585
Steven Thon <steventhon94@gmail.com>
2024-02-19 11:14:25 -0800

    Complete 2.5, declaring Course component as a separate Module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,40 +1,4 @@
-const Header = (props) => {
-  return (
-    <h2>{props.text}</h2>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(part => 
-        <Part key={part.id} part={part} />
-      )}
-    </>
-  )
-}
-
-const Course = (props) => {
-  const total = props.course.parts.reduce((acc, p) => {
-    console.log('what is happening', acc, p)
-    return acc + p.exercises
-  }, 0,)
-  return (
-    <>
-      <Header text={props.course.name} />
-      <Content parts={props.course.parts} />
-      <b>total of {total} exercises</b>
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
