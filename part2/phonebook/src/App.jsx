9f67bc8643897258d820ef82001783a9e1c3a74b
Steven Thon <steventhon94@gmail.com>
2024-02-19 13:52:13 -0800

    Complete 2.6, adding and displaying names

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,52 @@
 import { useState } from 'react'
 
+const Person = (props) => {
+  return <li>{props.name}</li>
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: 
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+          <Person key={person.name} name={person.name} />
+        )}
+      </ul>
+      <div>debug newName: {newName}</div>
     </div>
   )
 }
