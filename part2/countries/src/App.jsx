04c54e04e51fd07500b8108b7d661af9755879a7
Steven Thon <steventhon94@gmail.com>
2024-02-25 18:45:14 -0800

    Complete 2.19, Add show button, which when pressed shows the view for that country

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,37 +2,45 @@ import { useState, useEffect } from 'react'
 
 import axios from 'axios'
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, toggleCountryData }) => {
   console.log('countries', countries)
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>
   } else if (countries.length === 1) {
-    return <CountryData country={countries[0]} />
+    return <CountryData country={{ ...countries[0], isExpanded: true }} />
   }
   return countries.map(country =>
-    <Country name={country.name.common} />
+    <Country country={country} toggleCountryData={toggleCountryData} />
   )
 }
 
-const Country = ({ name }) => {
-  return <div>{name}</div>
+const Country = ({ country, toggleCountryData }) => {
+  return (
+    <div>
+      {country.name.common}
+      <button onClick={() => toggleCountryData(country)}>{country.isExpanded ? "unshow" : "show"}</button>
+      <CountryData country={country} />
+    </div>
+  )
 }
 
 const CountryData = ({ country }) => {
-  return (
-    <>
-      <h1>{country.name.common}</h1>
-      <div>capital {country.capital}</div>
-      <div>area {country.area}</div>
-      <h3>languages:</h3>
-      <ul>
-        {Object.values(country.languages).map(language => {
-          return <li>{language}</li>
-        })}
-      </ul>
-      <img src={country.flags.png} alt={country.flags.alt} />
-    </>
-  )
+  if (country.isExpanded) {
+    return (
+      <>
+        <h1>{country.name.common}</h1>
+        <div>capital {country.capital}</div>
+        <div>area {country.area}</div>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country.languages).map(language => {
+            return <li>{language}</li>
+          })}
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt} />
+      </>
+    )
+  }
 }
 
 function App() {
@@ -49,7 +57,7 @@ function App() {
         .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then(response => {
           console.log('response', response)
-          setCountries(response.data.filter(c => c.name.common.toLowerCase().includes(value.toLowerCase())))
+          setCountries(response.data.filter(c => c.name.common.toLowerCase().includes(value.toLowerCase())).map(c => { return { ...c, isExpanded: false } }))
         })
     }
   }, [value])
@@ -58,13 +66,22 @@ function App() {
     setValue(event.target.value)
   }
 
+  const toggleCountryData = (country) => {
+    setCountries(countries.map(c => {
+      if (c.name.common === country.name.common) {
+        return {...c, isExpanded: !country.isExpanded}
+      }
+      return c
+    }))
+  }
+
   return (
     <>
       <div>
         find countries
         <input value={value} onChange={handleChange} />
       </div>
-      <Countries countries={countries}/>
+      <Countries countries={countries} toggleCountryData={toggleCountryData}/>
     </>
   )
 }
