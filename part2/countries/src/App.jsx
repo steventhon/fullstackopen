013836136f8acac9c3a7d98490a132d4ebb023b0
Steven Thon <steventhon94@gmail.com>
2024-02-27 19:05:56 -0800

    Complete 2.20, Add weather data

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,13 +4,17 @@ import axios from 'axios'
 
 const Countries = ({ countries, toggleCountryData }) => {
   console.log('countries', countries)
+  console.log('countries.length', countries.length)
   if (countries.length > 10) {
+    console.log('countries length > 10')
     return <div>Too many matches, specify another filter</div>
   } else if (countries.length === 1) {
-    return <CountryData country={{ ...countries[0], isExpanded: true }} />
+    console.log('countries length === 1')
+    return <CountryData country={countries[0]} />
   }
+  console.log('countries length < 10 and > 1')
   return countries.map(country =>
-    <Country country={country} toggleCountryData={toggleCountryData} />
+    <Country key={country.name.common} country={country} toggleCountryData={toggleCountryData} />
   )
 }
 
@@ -34,10 +38,14 @@ const CountryData = ({ country }) => {
         <h3>languages:</h3>
         <ul>
           {Object.values(country.languages).map(language => {
-            return <li>{language}</li>
+            return <li key={language}>{language}</li>
           })}
         </ul>
         <img src={country.flags.png} alt={country.flags.alt} />
+        <h3>Weather in {country.capital}</h3>
+        <div>temperature {country.capitalInfo.weather.temperature} Celsius</div>
+        <img src={country.capitalInfo.weather.icon} alt={country.capitalInfo.weather.alt} />
+        <div>wind {country.capitalInfo.weather.wind} m/s</div>
       </>
     )
   }
@@ -46,6 +54,7 @@ const CountryData = ({ country }) => {
 function App() {
   const [value, setValue] = useState('')
   const [countries, setCountries] = useState([])
+  const api_key = import.meta.env.VITE_SOME_KEY
 
   useEffect(() => {
     console.log('effect run, country is now', value)
@@ -57,7 +66,15 @@ function App() {
         .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then(response => {
           console.log('response', response)
-          setCountries(response.data.filter(c => c.name.common.toLowerCase().includes(value.toLowerCase())).map(c => { return { ...c, isExpanded: false } }))
+          const filteredCountries = response.data.filter(c => c.name.common.toLowerCase().includes(value.toLowerCase())).map(c => { return { ...c, isExpanded: false } })
+          console.log('filteredCountries.length', filteredCountries.length)
+          if (filteredCountries.length === 1) {
+            console.log('filteredCountries 1')
+            getWeatherDataSingle({ ...filteredCountries[0], isExpanded: true })
+          } else {
+            console.log('filteredCountries > 1')
+            setCountries(filteredCountries)
+          }
         })
     }
   }, [value])
@@ -66,13 +83,43 @@ function App() {
     setValue(event.target.value)
   }
 
+  const getWeatherDataSingle = (country) => {
+    console.log('getWeatherDataSingle for', country)
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&units=metric&appid=${api_key}`)
+      .then(response => {
+        console.log('response', response)
+        country.capitalInfo.weather = {
+          temperature: response.data.main.temp,
+          wind: response.data.wind.speed,
+          icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+          alt: response.data.weather[0].main }
+          setCountries([country])
+      })
+  }
+
+  const getWeatherData = (country) => {
+    console.log('getWeatherData for', country)
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&units=metric&appid=${api_key}`)
+      .then(response => {
+        console.log('response', response)
+        country.capitalInfo.weather = {
+          temperature: response.data.main.temp,
+          wind: response.data.wind.speed,
+          icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+          alt: response.data.weather[0].main }
+          setCountries(countries.map(c => c.name.common === country.name.common ? country : c ))
+      })
+  }
+
   const toggleCountryData = (country) => {
-    setCountries(countries.map(c => {
-      if (c.name.common === country.name.common) {
-        return {...c, isExpanded: !country.isExpanded}
-      }
-      return c
-    }))
+    let changedCountry = { ...country, isExpanded: !country.isExpanded }
+    if (changedCountry.isExpanded) {
+      getWeatherData(changedCountry)
+    } else {
+      setCountries(countries.map(c => c.name.common === country.name.common ? changedCountry : c ))
+    }
   }
 
   return (
